281b9c497dca33877ccb3c700faebe0bc31332bb
Lucas Reis <lucas.oreis@tvglobo.com.br>
2020-03-12 12:25:31 -0300

    creating a button with style from design system

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="buttons">
+      <h1>Buttons</h1>
+      <m-button>Sign up</m-button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { MButton } from "./components";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    MButton
   }
 };
 </script>
@@ -21,8 +23,16 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px 60px 0 60px;
+}
+
+#buttons h1 {
+  font-size: 60px;
+  color: #443665;
+  border-bottom: 1px;
+  border-bottom-color: #707070;
+  border-bottom-style: dashed;
+  padding-bottom: 30px;
 }
 </style>
---
+++ src/components/MButton/MButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button class="m_btn">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "m-button",
+  props: {
+    small: Boolean
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.m_btn {
+  background: #6524FB 0% 0% no-repeat padding-box;
+  width: 98px;
+  height: 42px;
+  color: #fff;
+  font-size: 14px;
+  letter-spacing: 0;
+  border: 0;
+  border-radius: 100px;
+  outline: 0;
+}
+
+.m_btn:hover {
+  background: #B278F8 0% 0% no-repeat padding-box;
+}
+
+.m_btn:active {
+  background: #A255FC 0% 0% no-repeat padding-box;
+}
+
+.m_btn:disabled {
+  background: #F1EAFF 0% 0% no-repeat padding-box;
+  color: #C4B9DB;
+}
+
+.m_btn::-moz-focus-inner {
+  border: 0;
+}
+</style>
